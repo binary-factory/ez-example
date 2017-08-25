407d473cbfbc9eed957886f0e5542248a2f7f48f
Oliver Haase <oliver.koeln@gmail.com>
2017-08-25 16:02:44 +0200

    first attempt of decorators

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,50 @@
-console.log('hello world');
+import 'reflect-metadata';
+
+import {
+    controller,
+    Controller,
+    EzContext,
+    EzPluginManager,
+    EzRouter,
+    EzRouterPlugin,
+    EzServer,
+    get,
+    Type
+} from '@binary-factory/ez-http';
+import { Container } from 'inversify';
+
+const routerPlugin = new EzRouterPlugin();
+EzPluginManager.registerPlugin(routerPlugin).then(() => {
+    console.log(EzPluginManager.plugins);
+});
+
+
+const router = new EzRouter();
+router.get( '/',() => {
+    console.log('root');
+});
+
+const server = new EzServer();
+server.use(router);
+server.listen(8080);
+
+
+@controller('/test')
+class Test implements Controller {
+    constructor() {
+        console.log('test created.');
+    }
+
+    @get()
+    test(context: EzContext) {
+        context.json({ status: 'ok' });
+    }
+}
+
+// set up container
+let container = new Container();
+
+// note that you *must* bind your controllers to Controller
+container.bind<Controller>(Type.Controller).to(Test).whenTargetNamed('TestController');
+
+server.registerContainer(<any>container);
